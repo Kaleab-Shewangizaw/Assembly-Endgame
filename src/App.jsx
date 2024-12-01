669ca796ffa1068bf757c44906efc7dc3d30d6e7
Kaleab-Shewangizaw <161711086+Kaleab-Shewangizaw@users.noreply.github.com>
2024-12-01 14:44:30 +0300

    status

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+export default function App() {
+  return (
+    <>
+    <main>
+        <header>
+            <h1>Assembly: Endgame</h1>
+                <p>Guess the word within 8 attempts to keep the 
+                programming world safe from Assembly!</p>
+        </header>
+        <section className = 'game-status'>
+            <h2>you win!</h2>
+            <p>Wll done! 🎉</p>
+        </section>
+
+    </main>
+    </>
+  );
+}
